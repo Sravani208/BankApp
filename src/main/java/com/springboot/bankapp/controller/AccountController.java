d4a453d953ca945e0b6ad1275c9f9a1f8b9245ef
unknown <sravanimaddela99@gmail.com>
2022-01-10 19:12:07 +0530

    backend connection

--- src/main/java/com/springboot/bankapp/controller/AccountController.java
+++ src/main/java/com/springboot/bankapp/controller/AccountController.java
@@ -1,13 +1,13 @@
-package com.springboot.api.microservices.api.controller;
+package com.springboot.bankapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+
 	@GetMapping("/test")
 	public String test() {
-		return ("workss!!!");
+		return "works!!";
 	}
-
 }
